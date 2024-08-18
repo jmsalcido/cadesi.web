946140759fe121f2032791c907d8f842d20e027a
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-17 18:55:27 -0700

    format files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import {Hero} from "@/app/components/hero";
 import {Header} from "@/app/components/header/header.component";
 
 export default function Home() {
-    return (
-        <main className="bg-white">
-                <Header/>
-                <Hero/>
-        </main>
-);
+  return (
+    <main className="bg-white">
+      <Header/>
+      <Hero/>
+    </main>
+  );
 }
