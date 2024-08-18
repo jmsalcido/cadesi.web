df536d9e2837d8d0a53d6ead931d49804bda70a6
Jose Salcido <jose@tepache.dev>
2024-08-17 18:27:35 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Hero} from "@/app/components/hero";
+import {Header} from "@/app/components/header";
+
+export default function Home() {
+    return (
+        <main className="bg-white">
+                <Header/>
+                <Hero/>
+        </main>
+);
+}
