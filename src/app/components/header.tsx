9161900fec62a02cac4db85b1dbd95d762e0bcac
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-17 18:52:40 -0700

    fix header.tsx with better icons and nav

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,90 +1,87 @@
+'use client';
+
 import React from "react";
+import {useState} from 'react'
+import {Dialog, DialogPanel} from '@headlessui/react'
+import {Bars3Icon, XMarkIcon, HomeIcon} from '@heroicons/react/24/outline'
+
+const navigation = [
+  {name: 'Quiénes Somos', href: '#'},
+  {name: 'Miembros', href: '#'},
+  {name: 'Próximos Proyectos', href: '#'},
+  {name: 'Aliados', href: '#'},
+  {name: 'Contacto', href: '#'},
+]
 
 export const Header: React.FC = () => {
-    return (
-        <header className="absolute inset-x-0 top-0 z-50">
-            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-                <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">CADESI</span>
-                        <img className="h-8 w-auto"
-                             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-                    </a>
-                </div>
-                <div className="flex lg:hidden">
-                    <button type="button"
-                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                             stroke="currentColor" aria-hidden="true">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                        </svg>
-                    </button>
-                </div>
-                <div className="hidden lg:flex lg:gap-x-12">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Quienes Somos</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Miembros</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Proximos Proyectos</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Aliados</a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Contacto</a>
-                </div>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span
-                        aria-hidden="true">&rarr;</span></a>
-                </div>
-            </nav>
-            {/*<div className="lg:hidden" role="dialog" aria-modal="true">*/}
-            {/*    <div className="fixed inset-0 z-50"></div>*/}
-            {/*    <div*/}
-            {/*        className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">*/}
-            {/*        <div className="flex items-center justify-between">*/}
-            {/*            <a href="#" className="-m-1.5 p-1.5">*/}
-            {/*                <span className="sr-only">Your Company</span>*/}
-            {/*                <img className="h-8 w-auto"*/}
-            {/*                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>*/}
-            {/*            </a>*/}
-            {/*            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">*/}
-            {/*                <span className="sr-only">Close menu</span>*/}
-            {/*                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"*/}
-            {/*                     stroke="currentColor" aria-hidden="true">*/}
-            {/*                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>*/}
-            {/*                </svg>*/}
-            {/*            </button>*/}
-            {/*        </div>*/}
-            {/*        <div className="mt-6 flow-root">*/}
-            {/*            <div className="-my-6 divide-y divide-gray-500/10">*/}
-            {/*                <div className="space-y-2 py-6">*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">*/}
-            {/*                        Quienes Somos*/}
-            {/*                    </a>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">*/}
-            {/*                        Miembros*/}
-            {/*                    </a>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">*/}
-            {/*                        Proximos Proyectos*/}
-            {/*                    </a>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">*/}
-            {/*                        Aliados*/}
-            {/*                    </a>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">*/}
-            {/*                        Contacto*/}
-            {/*                    </a>*/}
-            {/*                </div>*/}
-            {/*                <div className="py-6">*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log*/}
-            {/*                        in</a>*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-        </header>
-    );
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <HomeIcon aria-hidden="true" className="h-6 w-6"/>
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Abrir Menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6"/>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <div className="fixed inset-0 z-50"/>
+        <DialogPanel
+          className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6"/>
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
 }
