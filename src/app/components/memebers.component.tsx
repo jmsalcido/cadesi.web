8f89bb01b6d59da9fd46f88b18c7a66dbfbc9d82
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-17 19:27:53 -0700

    add some other sections

---
+++ src/app/components/memebers.component.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface Props {
+  coffeeShops: {
+    name: string;
+    imageUrl: string;
+  }[];
+}
+
+export const Members: React.FC<Props> = (props) => {
+  const {coffeeShops} = props;
+  return (
+    <div id={"miembros"} className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Miembros
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Los miembros de CADESI son los líderes en la industria del café en el desierto de Sonora.
+          </p>
+        </div>
+        <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+          {coffeeShops.map((coffeeShop) => (
+            <li key={coffeeShop.name}>
+              <div className="flex items-center gap-x-6">
+                <img alt="" src={coffeeShop.imageUrl} className="h-16 w-16 rounded-full"/>
+                <div>
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{coffeeShop.name}</h3>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 import {Hero} from "@/app/components/hero.component";
 import {Header} from "@/app/components/header.component";
+import {Members} from "@/app/components/memebers.component";
+import {Section} from "@/app/components/section.component";
 
 const navigation = [
-  {name: 'Quiénes Somos', href: '#'},
-  {name: 'Miembros', href: '#'},
+  {name: 'Quiénes Somos', href: '#quienes-somos'},
+  {name: 'Miembros', href: '#miembros'},
   {name: 'Próximos Proyectos', href: '#'},
   {name: 'Aliados', href: '#'},
   {name: 'Contacto', href: '#'},
 ]
+
+const coffeeShops = [
+  {
+    name: 'Josefina Café',
+    imageUrl: 'https://placehold.co/800x800'
+  },
+  {
+    name: 'Dante y Nova',
+    imageUrl: 'https://placehold.co/800x800'
+  },
+  {
+    name: 'Culto al Perro Café',
+    imageUrl: 'https://placehold.co/800x800'
+  },
+  {
+    name: 'El Chapucero',
+    imageUrl: 'https://placehold.co/800x800'
+  },
+];
 
 export default function Home() {
   return (
@@ -15,6 +36,14 @@ export default function Home() {
       <Header navigation={navigation}/>
       <Hero header={'CADESI'}
             subheader={'Colectivo de Café del Desierto'}/>
+      <Section header={navigation[0].name}
+               id={'quienes-somos'}
+               backgroundUrl={'https://i.imgur.com/vHiGXaT.png'}
+               description={'Somos un colectivo que promueve, enaltece y celebra la cultura del café en el desierto de Sonora.\n' +
+                 '\n' +
+                 'Cafeterías, casas tostadoras, baristas y fans del café, ¡bienvenidos!\n'}
+      />
+      <Members coffeeShops={coffeeShops}/>
     </main>
   );
 }
