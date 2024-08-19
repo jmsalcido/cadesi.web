42731c19c3acaa87b3203a892d8e7df5d8ab836d
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-18 21:38:40 -0700

    add link to the gform

--- src/app/components/section.component.tsx
+++ src/app/components/section.component.tsx
@@ -3,22 +3,22 @@ import React from "react";
 interface Props {
   id?: string;
   header: string;
-  description: string;
   backgroundUrl?: string;
   maskColor?: string;
   textColor?: string;
   height?: string;
+  children?: React.ReactNode;
 }
 
 export const Section: React.FC<Props> = (props) => {
   const {
     id,
     header,
-    description,
     backgroundUrl,
     maskColor = '--primary-color-rgb-mask',
     textColor = 'text-secondary',
     height = 'min-h-screen',
+    children,
   } = props;
 
   return (
@@ -34,27 +34,9 @@ export const Section: React.FC<Props> = (props) => {
           <h2 className={`text-4xl font-bold tracking-tight ${textColor} sm:text-6xl`}>
             {header}
           </h2>
-          <p className={`mt-6 text-lg leading-8 ${textColor}`}>
-            {description}
-          </p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div
-            className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            {/*{links.map((link) => (*/}
-            {/*  <a key={link.name} href={link.href}>*/}
-            {/*    {link.name} <span aria-hidden="true">&rarr;</span>*/}
-            {/*  </a>*/}
-            {/*))}*/}
-          </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {/*{stats.map((stat) => (*/}
-            {/*  <div key={stat.name} className="flex flex-col-reverse">*/}
-            {/*    <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>*/}
-            {/*    <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>*/}
-            {/*  </div>*/}
-            {/*))}*/}
-          </dl>
+          {children}
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,35 +20,59 @@ export default function Home() {
                textColor={'text-white'}
                height={'50vh'}
                backgroundUrl={'https://res.cloudinary.com/otfusion/image/upload/v1723948290/vHiGXaT_vdzdog.jpg'}
-               description={'Somos un colectivo que promueve, enaltece y celebra la cultura del café en el desierto de Sonora.\n' +
-                 '\n' +
-                 'Cafeterías, casas tostadoras, baristas y fans del café, ¡bienvenidos!\n'}
-      />
-      <Members />
+      >
+        <p className={`mt-6 text-3xl leading-8 text-gray-100`}>
+          Somos un colectivo que promueve, enaltece y celebra la cultura del café en el desierto de Sonora.
+        </p>
+        <p className={`mt-6 text-3xl leading-8 text-gray-100`}>
+          Cafeterías, casas tostadoras, baristas y fans del café, ¡bienvenidos!
+        </p>
+      </Section>
+      <Members/>
       <Section id={'proximos-proyectos'}
                header={navigation[2].name}
                height={'50vh'}
                maskColor={'--secondary-color-rgb-mask'}
                textColor={'text-white'}
-               description={'Pasaporte del Café (imagen) Registra a tu cafetería aquí (google form)\n' +
-                 'CADESI Talks: tertulias informales para hablar de un tema específico del café \n' +
-                 'Colecta para Casas Hogares en diciembre\n' +
-                 'Festival del Café 2025\n'}
-      />
+      >
+        <ul className={`mt-6 text-3xl leading-8 text-gray-100 list-disc list-inside`}>
+          <li className="my-5">
+            <a href={'https://forms.gle/CMbWYrHaEdf8MhQd7'}>
+              Pasaporte del Café - Registra a tu cafetería!
+            </a>
+          </li>
+          <li className="my-5">CADESI Talks: tertulias informales para hablar de un tema específico del café</li>
+          <li className="my-5">Colecta para Casas Hogares en Diciembre</li>
+          <li className="my-5">Festival del Café 2025</li>
+        </ul>
+      </Section>
       <Section id={'aliados'}
                header={'Aliados'}
                height={'50vh'}
                textColor={'text-gray-900'}
-               description={'¿Te interesa dar algún curso o taller sobre un tema de café? ¿Quieres vender el Pasaporte del Café de Hermosillo en tu cafetería? ¿Quieres apoyar como voluntario en los eventos? ¡Contáctanos!\n'}
-      />
+      >
+        <p className={`mt-6 text-3xl leading-8 text-gray-900`}>
+          ¿Te interesa dar algún curso o taller sobre un tema de café?
+        </p>
+        <p className={`mt-6 text-3xl leading-8 text-gray-900`}>
+          ¿Quieres vender el Pasaporte del Café de Hermosillo en tu cafetería?
+        </p>
+        <p className={`mt-6 text-3xl leading-8 text-gray-900`}>
+          ¿Quieres apoyar como voluntario en los eventos?
+        </p>
+        <p className={`mt-6 text-3xl leading-8 text-gray-900`}>
+          ¡Contáctanos!
+        </p>
+      </Section>
       <Section id={'contacto'}
                header={'Contacto'}
                height={'50vh'}
                maskColor={'--secondary-color-rgb-mask'}
-               textColor={'text-white'}
-               description={'cadesihmo@gmail.com\n' +
-                 '@cadesisonora\n'}
-      />
+               textColor={'text-white'}>
+        <div className="flex flex-col justify-center items-center text-3xl">
+          <a href={'mailto:cadesihmo@gmail.com'}>Email</a>
+        </div>
+      </Section>
     </main>
   );
 }
