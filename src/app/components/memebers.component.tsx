67b5096318ae651bc597ce02c0b0260bcd8b0b80
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-19 09:46:34 -0700

    update copy

--- src/app/components/memebers.component.tsx
+++ src/app/components/memebers.component.tsx
@@ -30,7 +30,7 @@ export const Members: React.FC<Props> = (props) => {
             Miembros
           </h2>
           <p className="mt-6 text-xl leading-8 text-gray-600">
-            Los miembros de CADESI son los líderes en la industria del café en el desierto de Sonora.
+            Los miembros promueven y celebran la cultura del café en el desierto de Sonora.
           </p>
         </div>
         <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
