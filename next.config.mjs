31b5c583d718af4eaa552247774adf899b9e297d
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-18 23:20:38 -0700

    fix package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import {resolve, dirname} from 'path';
+import {dirname, resolve} from 'path';
 import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +10,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias['@'] = resolve(__dirname, 'src');
     return config;
-  },
-}
+  }
+};
 
 export default nextConfig;
