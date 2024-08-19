44270e35b97cdf22807131b16224d8244ccf7e07
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-18 19:58:48 -0700

    update entire site with colors and sections

--- src/app/components/memebers.component.tsx
+++ src/app/components/memebers.component.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const Members: React.FC<Props> = (props) => {
   const {coffeeShops} = props;
   return (
-    <div id={"miembros"} className="bg-cadesi py-24 sm:py-32 text-cadesi-font">
+    <div id={"miembros"} className="bg-cadesi py-24 sm:py-32 text-gray-900">
       <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
         <div className="max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
@@ -23,7 +23,7 @@ export const Members: React.FC<Props> = (props) => {
         <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
           {coffeeShops.map((coffeeShop) => (
             <li key={coffeeShop.name}>
-              <div className="flex items-center gap-x-6">
+              <div className="flex items-center gap-x-6 text-gray-900">
                 <img alt="" src={coffeeShop.imageUrl} className="h-32 w-32 rounded-full"/>
                 <div>
                   <h3 className="text-2xl font-semibold leading-7 tracking-tight">{coffeeShop.name}</h3>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,8 @@ export default function Home() {
       <Hero/>
       <Section id={'quienes-somos'}
                header={navigation[0].name}
-               maskColor={'--primary-color-rgb-mask'}
+               maskColor={'--secondary-color-rgb-mask'}
+               textColor={'text-white'}
                height={'50vh'}
                backgroundUrl={'https://res.cloudinary.com/otfusion/image/upload/v1723948290/vHiGXaT_vdzdog.jpg'}
                description={'Somos un colectivo que promueve, enaltece y celebra la cultura del café en el desierto de Sonora.\n' +
@@ -46,6 +47,8 @@ export default function Home() {
       <Section id={'proximos-proyectos'}
                header={navigation[2].name}
                height={'50vh'}
+               maskColor={'--secondary-color-rgb-mask'}
+               textColor={'text-white'}
                description={'Pasaporte del Café (imagen) Registra a tu cafetería aquí (google form)\n' +
                  'CADESI Talks: tertulias informales para hablar de un tema específico del café \n' +
                  'Colecta para Casas Hogares en diciembre\n' +
@@ -54,6 +57,7 @@ export default function Home() {
       <Section id={'aliados'}
                header={'Aliados'}
                height={'50vh'}
+               textColor={'text-gray-900'}
                description={'¿Te interesa dar algún curso o taller sobre un tema de café? ¿Quieres vender el Pasaporte del Café de Hermosillo en tu cafetería? ¿Quieres apoyar como voluntario en los eventos? ¡Contáctanos!\n'}
       />
     </main>
