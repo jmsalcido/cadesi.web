84289cf56182626eea96700382c3c602a3d6cc84
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-18 22:41:38 -0700

    add deploy and posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import type {Metadata} from "next";
 import {Inter, Roboto} from "next/font/google";
 import "./globals.css";
+import {PHProvider} from "@/app/providers";
+import dynamic from "next/dynamic";
 
 const inter = Inter({subsets: ["latin"]});
 const roboto = Roboto({
@@ -16,6 +18,10 @@ export const metadata: Metadata = {
   description: "Cafe en el desierto de Sonora",
 };
 
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+  ssr: false,
+})
+
 export default function RootLayout({
                                      children,
                                    }: Readonly<{
@@ -23,7 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${roboto.className}`}>{children}</body>
+      <PHProvider>
+        <body className={`${inter.className} ${roboto.className}`}>
+          <PostHogPageView />
+          {children}
+        </body>
+      </PHProvider>
     </html>
   );
 }
