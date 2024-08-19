eff32dd06d5184e60bf1e8fdd70bcd0ab8f73772
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-18 22:52:11 -0700

    add config for @ imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import {resolve, dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = resolve(__dirname, 'src');
+    return config;
+  },
+}
 
 export default nextConfig;
